af3cf6acf2c2e16d80ede6627fb4b3ba5d481e50
lipenghui <abc@163.com>
2018-04-24 15:19:16 +0800

    搜索框布局

--- src/components/ShoppingMall.vue
+++ src/components/ShoppingMall.vue
@@ -1,14 +1,69 @@
 <template>
     <div>
+        <!--search bar layout-->
+        <div class='search-bar'>
+            <van-row style='height: 100%;'>
+                <van-col span='3' class='location-icon'>
+                    <img :src='locationIcon'>
+                </van-col>
+                <van-col span='16' class='search-input'>
+                    <input type='text' placeholder='查找'/>
+                </van-col>
+                <van-col span='5' class='seek'>
+                    <van-button size='mini' class='button'>查找</van-button>
+                </van-col>
+            </van-row>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        data() {
+            return {
+                locationIcon: require('../assets/images/location.png')
+            }
+        }
+    }
 </script>
 
-<style scoped>
-
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+    @import '~common/stylus/variable'
+    input::-webkit-input-placeholder
+        color $color-background
+        font-size $font-size-s
+    .search-bar
+        width 100%
+        height 1rem
+        background-color $color-search-background
+        .location-icon
+            height 100%
+            position relative
+            padding .15rem
+            img
+                display block
+                width 100%
+                height 100%
+        .search-input
+            height 100%
+            position relative
+            input
+                border none
+                outline none
+                background-color $color-search-background
+                height .8rem
+                width 100%
+                border-bottom 1px solid $color-background
+                position absolute
+                left 0
+                top 50%
+                margin-top -.4rem
+                color $color-background
+        .seek
+            height 100%
+            padding .2rem
+            .button
+                width 100%
+                height 100%
+                font-size $font-size-s
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
+import {Button, Row, Col} from 'vant'
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
 fastclick.attach(document.body)
+
+Vue.use(Button).use(Row).use(Col)
 
 Vue.use(VueLazyload, {
     loading: require('./common/image/default.png')
